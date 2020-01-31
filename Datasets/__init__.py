d6ae9029cda0256e94a1ae2fa8ad90114870961a
backmag <mjgsio7d>
2020-01-31 14:59:14 +0100

    Created cleaning methods for the financial news-dataset.

--- Datasets/__init__.py
+++ Datasets/__init__.py
@@ -1,8 +1,16 @@
-from Datasets.DataFormatter import DataFormatter
-import os
+from Datasets import Utilities
 from pathlib import Path
-cwd = Path.cwd().parent.parent.parent
-data_path = cwd / 'Datasets' / 'financial-news-dataset-master' / 'financial-news-dataset-master' / 'ReutersNews106521' /\
-            '20061020'
-df = DataFormatter(data_path)
-df.format_data()
+
+
+def main():
+    extract_data = False
+    if extract_data:
+        nbr_subsets = 1
+        data_path = Path("C:/Users/gusta/Documents/Skola/Lund/Indek år "
+                         "5/Exjobb/Datasets/financial-news-dataset-master/financial-news-dataset-master/ReutersNews106521")
+        Utilities.format_data(data_path, nbr_subsets)
+    data = Utilities.load_file("financial_news_dataset_20061020-20131119")
+
+
+if __name__ == '__main__':
+    main()
