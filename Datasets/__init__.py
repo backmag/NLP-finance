56ee8f0f6d2c0d09621c8b813561e6f9f6cf4d6f
backmag <mjgsio7d>
2020-02-03 10:12:03 +0100

    Uploaded data of treasury rates (1 and 3 year) and S&P-index as a pandas dataframe.

--- Datasets/__init__.py
+++ Datasets/__init__.py
@@ -1,5 +1,7 @@
 from Datasets import Utilities
 from pathlib import Path
+import pandas as pd
+import os
 
 
 def main():
@@ -7,9 +9,13 @@ def main():
     if extract_data:
         nbr_subsets = 1
         data_path = Path("C:/Users/gusta/Documents/Skola/Lund/Indek år "
-                         "5/Exjobb/Datasets/financial-news-dataset-master/financial-news-dataset-master/ReutersNews106521")
+                         "5/Exjobb/Datasets/financial-news-dataset-master/financial-news-dataset-master"
+                         "/ReutersNews106521")
         Utilities.format_data(data_path, nbr_subsets)
-    data = Utilities.load_file("financial_news_dataset_20061020-20131119")
+    news_data = Utilities.load_file(str(os.getcwd() + "/data/financial_news_dataset_20061020-20131119"))
+    print("News data loaded successfully.")
+    ts_data = pd.read_pickle(Path(os.getcwd() + "/data/stock_data.pkl"))
+    print("Stock data loaded successfully. ")
 
 
 if __name__ == '__main__':
